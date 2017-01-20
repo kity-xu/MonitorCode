eb4277394ed351684873c47ee6651fb470a9dcdc
xuxiaodong <474493616@qq.com>
2017-01-20 10:15:39 +0800

    add socketServer

--- src/haina.im/monitor/monitor_centre/centre.go
+++ src/haina.im/monitor/monitor_centre/centre.go
@@ -8,7 +8,7 @@ import (
 )
 
 func main() {
-	notif := &protocol.Notification{}
+	//notif := &protocol.Notification{}
 	log, _ := seelog.LoggerFromConfigAsFile("haina.im/monitor/monitor_centre/config/logconfig.xml")
 	defer log.Flush()
 
@@ -18,13 +18,13 @@ func main() {
 
 	for {
 		//fmt.Println("---------MonitorNode:%v", server.Buffer)
-		if server.Buffer.Time != "" { //判断结构体是否为空，以防止取值越界
+		if server.IsConn && server.Buffer.Time != "" { //判断结构体是否为空，以防止取值越界
 			if len(server.Buffer.Arms) > 0 {
 				for _, arm := range server.Buffer.Arms {
-					armmsg += fmt.Sprintf("错误码：%d; 错误级别：%d; 错误信息：%s --时间：%s\n", arm.Code, arm.Grade, arm.Msg, server.Buffer.Time) + "\n"
+					armmsg += fmt.Sprintf("错误码：%d; 错误级别：%d; 错误信息：%s --时间：%s\n", arm.Code, arm.Grade, arm.Msg, server.Buffer.Time)
 				}
-				log.Error(armmsg)      //日志落地
-				notif.SendMail(armmsg) //发邮件
+				log.Error(armmsg) //日志落地
+				//notif.SendMail(armmsg) //发邮件
 				armmsg = ""
 				break
 			}
--- src/haina.im/monitor/monitor_centre/protocol/socketserver.go
+++ src/haina.im/monitor/monitor_centre/protocol/socketserver.go
@@ -1,11 +1,13 @@
 package protocol
 
 import (
+	//"container/list"
 	"encoding/json"
 	"fmt"
 	"github.com/cihub/seelog"
 	"log"
 	"net/http"
+	"strings"
 
 	"golang.org/x/net/websocket"
 )
@@ -51,39 +53,77 @@ type MonitorData struct {
 }
 
 type SocketServer struct {
-	Buffer    MonitorData
-	IsDisConn bool
+	Buffer MonitorData
+	IsConn bool
 }
 
-func (this *SocketServer) Echo(ws *websocket.Conn) {
-	log, _ := seelog.LoggerFromConfigAsFile("haina.im/monitor/monitor_centre/config/logconfig.xml")
-	defer log.Flush()
+type Client struct {
+	ws       *websocket.Conn
+	Addr     string
+	Data     MonitorData
+	LastData MonitorData
+}
 
-	var err error
-	this.Buffer = MonitorData{}
+var logf seelog.LoggerInterface
+
+var clients map[string]Client
+
+func (this *SocketServer) websocketServer(ws *websocket.Conn) {
+	var (
+		err       error
+		clientMsg string
+	)
+
+	id := ws.Request().RemoteAddr
+	ip := strings.Split(id, ":")[0]
+	node := Client{}
+	node.Addr = id
+	node.ws = ws
+
+	defer func() {
+		if err = ws.Close(); err != nil {
+			logf.Error("Websocket could not be closed", err.Error())
+		}
+
+		logf.Flush()
+		logf.Close()
+	}()
+
 	for {
-
-		var reply string
-
-		if err = websocket.Message.Receive(ws, &reply); err != nil {
-			this.IsDisConn = true
-			log.Debug("Can't receive")
-			break
+		if err = websocket.Message.Receive(ws, &clientMsg); err != nil {
+			logf.Debug(id, "receive disconnected...")
+			return
 		}
 
-		err := json.Unmarshal([]byte(reply), &this.Buffer)
-		msg := "googd luck xulang"
+		node.LastData = node.Data
+		if err := json.Unmarshal([]byte(clientMsg), &node.Data); err != nil {
+			logf.Error(id, " 反序列化错误...")
+			websocket.Message.Send(ws, "反序列化错误。。。")
+			return
+		}
+
+		fmt.Println("=----------addrIP:", id)
+		fmt.Println("=----------addrIP:", ip)
+
+		clients[ip] = node
+
+		msg := "good luck xulang"
 		if err = websocket.Message.Send(ws, msg); err != nil {
-			break
+			logf.Debug(id, "send disconnected...")
 		}
+		logf.Debug(clients)
+		fmt.Println(len(clients))
 	}
 }
 
 func (this *SocketServer) StartSocketserver() {
+	clients = make(map[string]Client)
+	logf, _ = seelog.LoggerFromConfigAsFile("haina.im/monitor/monitor_centre/config/logconfig.xml")
+
 	fmt.Println("begin")
 	http.Handle("/", http.FileServer(http.Dir("."))) // <-- note this line
 
-	http.Handle("/socket", websocket.Handler(this.Echo))
+	http.Handle("/socket", websocket.Handler(this.websocketServer))
 
 	if err := http.ListenAndServe(":5010", nil); err != nil {
 		log.Fatal("ListenAndServe:", err)
